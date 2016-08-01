8690ab5b2c4c58a0cf4343ba60c95706c17e3e42
房慧科技 <fanghuikejifanghuikeji@fanghuikejidemac-mini.local>
2016-08-01 17:12:27 +0800

    Initial Commit

---
+++ LiWuShuoTests/LiWuShuoTests.swift
@@ -0,0 +1,36 @@
+//
+//  LiWuShuoTests.swift
+//  LiWuShuoTests
+//
+//  Created by 房慧科技 on 16/8/1.
+//  Copyright © 2016年 fanghuikeji. All rights reserved.
+//
+
+import XCTest
+@testable import LiWuShuo
+
+class LiWuShuoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
